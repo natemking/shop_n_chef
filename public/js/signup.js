d0b79bc83acb8489ee81805fb40c7d292fe76e8e
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-19 20:43:46 -0500

    Added an error message to the login that if the username or password is wrong, the user is warned. Added a message to the user in recipe.js if there are no recipes returned

--- public/js/signup.js
+++ public/js/signup.js
@@ -36,8 +36,6 @@ $(document).ready(() => {
   }
 
   function handleLoginErr(err) {
-    console.log(err.responseJSON);
-    console.log(err);
     $("#alert .msg").text(err.responseJSON.errors[0].message);
     $("#alert").fadeIn(500);
   }
