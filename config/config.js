a859a6602a2b1d75b2b65876ce2ad99ac19c6aca
Nate M King <72567521+natemking@users.noreply.github.com>
2021-01-20 11:31:04 -0500

    Converted sequelize to use donenv

---
+++ config/config.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.LOCAL_USER,
+    password: process.env.LOCAL_PW,
+    database: process.env.LOCAL_DB,
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: "null",
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+};
